31c0291b1c17f9d76a0b33e20d58b8ae2bc71375
OpticalSpring <54758407+OpticalSpring@users.noreply.github.com>
2019-12-05 18:51:49 +0900

    갈아엎음

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public int eventNumber;
+    public bool eventProceeding;
+
+
+    public GameObject player;
+    public GameObject[] checkPos;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EventCheck();
+    }
+
+    void EventCheck()
+    {
+        if (eventProceeding == false)
+        {
+            switch (eventNumber)
+            {
+                case 0:
+                    StartCoroutine(Event_0());
+                    break;
+                case 1:
+                    if (Vector3.Distance(player.transform.position, checkPos[0].transform.position) < 2)
+                    {
+                        StartCoroutine(Event_1());
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+    }
+
+    
+
+    IEnumerator Event_0()
+    {
+        eventNumber++;
+        eventProceeding = true;
+        yield return new WaitForSeconds(6);
+        eventProceeding = false;
+    }
+    IEnumerator Event_1()
+    {
+        eventNumber++;
+        eventProceeding = true;
+        yield return new WaitForSeconds(3);
+        //sound_eff.enabled = true;
+        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(3);
+        eventProceeding = false;
+    }
+    IEnumerator Event_2()
+    {
+        yield return new WaitForSeconds(0);
+    }
+    IEnumerator Event_3()
+    {
+        yield return new WaitForSeconds(0);
+    }
+    IEnumerator Event_4()
+    {
+        yield return new WaitForSeconds(0);
+    }
+}
---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextManager : MonoBehaviour
+{
+    public GameObject panel;
+    public bool textProceeding;
+    
+
+    public void SetTextUp(Sprite image, float delayTime)
+    {
+        panel.SetActive(true);
+        panel.GetComponent<Image>().sprite = image;
+        panel.GetComponent<Image>().SetNativeSize();
+        textProceeding = true;
+        StartCoroutine(DelayOff(delayTime));
+    }
+
+    IEnumerator DelayOff(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        panel.SetActive(false);
+        textProceeding = false;
+    }
+}
