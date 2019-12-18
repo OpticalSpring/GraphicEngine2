1842a7aa7da5682bf9f975466d2f7b82f69e1b1a
OpticalSpring <54758407+OpticalSpring@users.noreply.github.com>
2019-12-18 18:18:51 +0900

    카메라워크

---
+++ Assets/Scripts/MoveCam.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    public GameObject point;
+    public float movementSpeed;
+    // Start is called before the first frame upd
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, point.transform.position,movementSpeed* Time.deltaTime);
+    }
+}
